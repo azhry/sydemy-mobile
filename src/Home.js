28be43fb4dd8927767f0319e144a1dfc293ca562
azhry <arliansyah_azhary@yahoo.com>
2018-06-14 21:57:49 +0700

    Home UI 80%

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
-import { FlatList, View } from 'react-native';
-import { Container, Content, Text, Icon } from 'native-base';
-import Toolbar from './components/Toolbar';
-import ImageItem from './components/ImageItem';
+import React, { Component 					} 	from 'react';
+import { FlatList, View, Dimensions 		} 	from 'react-native';
+import { Container, Content, Text, Icon 	} 	from 'native-base';
+import Toolbar 									from './components/Toolbar';
+import ImageItem 								from './components/ImageItem';
+import Carousel 								from 'react-native-snap-carousel';
+
+const deviceWidth = Dimensions.get('window').width;
 
 export default class Home extends Component {
 
@@ -12,26 +15,30 @@ export default class Home extends Component {
 		};
 	};
 
-	constructor( props ) {
+	constructor(props) {
 
-		super( props );
+		super(props);
 		this.state = {
 			data: [{
 				title: 'Insight Workshop',
 				description: 'Deskripsi singkat mengenai Workshop',
-				image: 'https://sydemy.com/home/../admission/admin/slider_banner/images/LeafnoPic.png'
+				image: 'https://sydemy.com/home/../admission/admin/slider_banner/images/LeafnoPic.png',
+				navigateTo: 'ProductScanner'
 			}, {
 				title: 'Intensive Class',
 				description: 'Deskripsi singkat mengenai Intensive Class',
-				image: 'https://sydemy.com/home/../admission/admin/slider_banner/images/slider_IC.jpg'
+				image: 'https://sydemy.com/home/../admission/admin/slider_banner/images/slider_IC.jpg',
+				navigateTo: 'ProductScanner'
 			}, {
 				title: 'Weekend Class',
 				description: 'Deskripsi singkat mengenai Weekend Class',
-				image: 'https://sydemy.com/home/../admission/admin/slider_banner/images/slider_WC.jpg'
+				image: 'https://sydemy.com/home/../admission/admin/slider_banner/images/slider_WC.jpg',
+				navigateTo: 'ProductScanner'
 			}, {
 				title: 'Shop',
 				description: 'Deskripsi singkat mengenai Shop',
-				image: 'http://placehold.it/200'
+				image: 'http://placehold.it/200',
+				navigateTo: 'ProductScanner'
 			}]
 		};
 
@@ -39,19 +46,32 @@ export default class Home extends Component {
 
 	render() {
 
+		const { navigate } = this.props.navigation;
+
 		return (
 			<Container>
 				<Content>
+					<Carousel
+						ref={c => { this._carousel = c; }}
+						data={this.state.data}
+						renderItem={({item, index}) => (
+							<ImageItem 
+								title={item.title}
+								image={item.image}/>
+						)}
+						sliderWidth={deviceWidth}
+						itemWidth={deviceWidth}/>
 					<View style={{ marginHorizontal: 6 }}>
 						<Text style={{ fontSize: 19 }}><Icon name="ios-ribbon"/> Our Services</Text>
 					</View>
 					<FlatList
 						numColumns={2}
-						data={ this.state.data }
-						renderItem={({ item }) => 
+						data={this.state.data}
+						renderItem={({ item }) =>
 							<ImageItem
-								title={ item.title }
-								image={ item.image }/>
+								title={item.title}
+								image={item.image}
+								navigateTo={() => navigate(item.navigateTo)}/>	
 						}
 						keyExtractor={( item, index ) => index.toString()}/>
 				</Content>
--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -1,5 +1,5 @@
 import React, { Component 		} 	from 'react';
-import { View 					} 	from 'react-native';
+import { View, TouchableNativeFeedback 	} 	from 'react-native';
 import { Text, Content, Card 	} 	from 'native-base';
 import { CachedImage 			} 	from 'react-native-img-cache';
 
@@ -12,12 +12,16 @@ export default class ImageItem extends Component {
 	render() {
 		return (
 			<Content style={{ marginHorizontal: 6, marginVertical: 4 }}>
+				<TouchableNativeFeedback
+					onPress={this.props.navigateTo}
+					background={TouchableNativeFeedback.SelectableBackground()}>
 				<Card style={{ borderRadius: 8 }}>
 					<CachedImage resizeMode="cover" style={{ width: '100%', height: 160, borderRadius: 8 }} source={{ uri: this.props.image }}/>
 					<View style={{ flex: 1, position: 'absolute', bottom: 0, left: 0, right: 0, top: 0, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.2)', borderRadius: 8 }}>
 						<Text style={{ color: 'white', fontSize: 18, textAlign: 'center' }}>{ this.props.title }</Text>
 					</View>
 				</Card>
+				</TouchableNativeFeedback>
 			</Content>
 		);
 	}
